dbd619599f2d60d2a42859c0bd526939481bc74d
Ben Toomer <mullet1989@gmail.com>
2016-07-14 19:02:23 +0100

    added redis

--- routes/athletes.js
+++ routes/athletes.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var redis = require("redis"),
+    client = redis.createClient(process.env.redis);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:name', function(req, res, next) {
+    client.set("name", req.params.name, redis.print);
     res.send("hello world");
 });
 
