78cab2e163509b0a8aa046c0617df4e4ad2f9889
benjamintoomer <mullet1989@gmail.com>
2016-07-16 21:55:26 +0100

    introduced gulp tasks at home

--- app/main.js
+++ app/main.js
@@ -2,3 +2,5 @@
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_component_1 = require('./app.component');
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
+
+//# sourceMappingURL=../maps/main.js.map
